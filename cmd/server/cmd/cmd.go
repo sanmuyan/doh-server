c12988818d81077e73f3ec59bd621105747421e2
sanmuyan <m@sanmuyan.com>
2025-03-04 18:10:52 +0800

    优化命令行参数

--- cmd/server/cmd/cmd.go
+++ cmd/server/cmd/cmd.go
@@ -14,13 +14,19 @@ import (
 	"runtime"
 )
 
-var cmdReady bool
+var rootCtx context.Context
 
 var rootCmd = &cobra.Command{
 	Use:   "doh-server",
 	Short: "DoH Server",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		err := initConfig()
+		if err != nil {
+			logrus.Fatalf("init config error: %v", err)
+		}
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		cmdReady = true
+		configpost.PostInit(rootCtx)
 	},
 	Example: "doh-server -c config.yaml",
 }
@@ -68,6 +74,16 @@ func initConfig() error {
 	// 配置文件和命令行参数都不指定时的默认配置
 	// viper.SetDefault("conn_timeout", 10)
 
+	// 设置默认配置文件，如果配置文件是可选的则可以不设置
+	//if len(configFile) == 0 {
+	//	dir, err := os.Getwd()
+	//	if err != nil {
+	//		return err
+	//	}
+	//	configFile = dir + "/config.yaml"
+	//	configFile = filepath.Clean(configFile)
+	//}
+
 	// 读取配置文件
 	if len(configFile) > 0 {
 		viper.SetConfigFile(configFile)
@@ -101,25 +117,13 @@ func initConfig() error {
 		gin.DefaultWriter = os.Stdout
 		logrus.SetReportCaller(true)
 	}
+	logrus.Debugf("config init completed: %+v", config.Conf)
 	return nil
 }
 
 func Execute(ctx context.Context) {
+	rootCtx = ctx
 	if err := rootCmd.Execute(); err != nil {
 		logrus.Fatalf("cmd execute error: %v", err)
 	}
-	if cmdReady {
-		err := initConfig()
-		if err != nil {
-			logrus.Fatalf("init config error: %v", err)
-		}
-
-		logrus.Debugf("config init completed: %+v", config.Conf)
-
-		initConfigPost(ctx)
-	}
-}
-
-func initConfigPost(ctx context.Context) {
-	configpost.PostInit(ctx)
 }
